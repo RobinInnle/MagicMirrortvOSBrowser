a0077f4c4d01eff426255723ff0140e35df7f2e4
RobinInnle <czuchnr@gmail.com>
2025-02-14 21:29:48 +1100

    Gutted the advanced features, kept the basics for the MagicMirror display.

--- _Project/Browser/ViewController.h
+++ _Project/Browser/ViewController.h
@@ -5,7 +5,6 @@
 //  Created by Steven Troughton-Smith on 20/09/2015.
 //  Improved by Jip van Akker on 14/10/2015 through 10/01/2019
 //
-
 #import <UIKit/UIKit.h>
 #import <GameKit/GameKit.h>
 
@@ -26,4 +25,3 @@
 
 
 @end
-
